187fc1c8365a86e0f9f78c696eb158f498216f75
nathanBarloy <nbarloy@gmail.com>
2025-03-08 17:40:05 +0100

    Scale of the player

--- monkey/player/player.gd
+++ monkey/player/player.gd
@@ -1,8 +1,11 @@
 extends Area2D
 
-@export var speed = 350  # speed (pixel/s)
+@export var speed = 400  # speed (pixel/s)
+@export var min_scale: float = 0.65
+@export var max_scale: float = 1.
 var screen_origin: Vector2
 var screen_size: Vector2
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -34,4 +37,18 @@ func _process(delta: float) -> void:
 	#	$AnimatedSprite2D.stop()
 	
 	position += velocity * delta
-	position = position.clamp(screen_origin, screen_size)
+	# Clamp up and down
+	if position.y <= screen_origin.y:
+		position.y = screen_origin.y
+	if position.y >= screen_origin.y + screen_size.y:
+		position.y = screen_origin.y + screen_size.y
+	
+	# Clamp left and right
+	if position.x <= screen_origin.x:
+		position.x = screen_origin.x
+	if position.x >= screen_origin.x + screen_size.x:
+		position.x = screen_origin.x + screen_size.x
+	
+	# Compute scale
+	var y_prop = (position.y - screen_origin.y) / screen_size.y
+	scale = Vector2.ONE * (min_scale + (max_scale - min_scale) * y_prop)
