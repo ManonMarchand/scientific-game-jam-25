f9beb13ec4de8c832a8a967f8108e85472733ab5
MARCHAND MANON <manonmarchand22@gmail.com>
2025-03-08 12:57:36 +0100

    feat: intruder node

--- monkey/panel/diagram_node.gd
+++ monkey/panel/diagram_node.gd
@@ -3,10 +3,16 @@ class_name DiagramNode
 
 extends StaticBody2D
 
+@export
+var is_trespaser_node: bool = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$ColorRect.modulate = Color(Color.AQUAMARINE, 0.5)
+	if not is_trespaser_node:
+		$ColorRect.modulate = Color(Color.AQUAMARINE, 0.5)
+	else:
+		$ColorRect.modulate = Color(Color.CRIMSON, 0.5)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
