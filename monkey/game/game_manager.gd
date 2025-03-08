6404878779325edc248699a243cb130a19aa9a48
MARCHAND MANON <manonmarchand22@gmail.com>
2025-03-08 21:16:02 +0100

    feat: intro + style

---
+++ monkey/game/game_manager.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+	elif event is InputEventKey and event.is_released() and event.physical_keycode == KEY_F:
+		var window := get_viewport().get_window()
+		window.mode = (
+			Window.MODE_FULLSCREEN if window.mode != Window.MODE_FULLSCREEN
+			else Window.MODE_WINDOWED
+		)
