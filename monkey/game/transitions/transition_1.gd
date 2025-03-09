5a6ac0db7bd9121b3ab439077802cdd051d79579
MARCHAND MANON <manonmarchand22@gmail.com>
2025-03-09 13:28:15 +0100

    transitions

---
+++ monkey/game/transitions/transition_1.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+func _ready():
+	$AnimatedSprite2D.play(&"default")
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.is_released():
+		print("transition done")
+		PlayerVariables.emit_signal("transition_done")
