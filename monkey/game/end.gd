0b18cc74c5b282bc0b6cc7f4b9153e88078c0e70
nathanBarloy <nbarloy@gmail.com>
2025-03-10 20:44:57 +0100

    Add reload label

--- monkey/game/end.gd
+++ monkey/game/end.gd
@@ -2,8 +2,15 @@ extends Node2D
 
 signal restart_game
 
+func _ready():
+	$Label.visible = false
+
 func _input(event: InputEvent) -> void:
 	if $Timer.is_stopped():
 		if event is InputEventKey:
 			if event.pressed:
 				get_tree().change_scene_to_file("res://game/intro.tscn")
+
+
+func _on_timer_timeout() -> void:
+	$Label.visible = true
