fbc8cbe5d44db788e21be408906cc3caf85a77e6
ManonMarchand <manonmarchand22@gmail.com>
2025-03-11 21:13:06 +0100

    add win/loose sounds

--- monkey/game/layton_lose.gd
+++ monkey/game/layton_lose.gd
@@ -4,3 +4,6 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.is_released():
 		PlayerVariables.emit_signal("replay_level")
 		
+func _ready() -> void:
+	$SonDefaite.play()
+		
--- monkey/game/layton_win.gd
+++ monkey/game/layton_win.gd
@@ -4,3 +4,5 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.is_released():
 		PlayerVariables.emit_signal("next_level")
 		
+func _ready() -> void:
+	$SonVictoire.play()
